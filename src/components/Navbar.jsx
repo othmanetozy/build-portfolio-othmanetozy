bb6e3afcbf06dae4e030cfc3e7b704aca7986198
othmane tozy <tozyothmane123@gmail.com>
2024-09-13 16:55:08 +0100

    finish services

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,15 @@ const Navbar = () => {
             <li className='navbar-item'>
                 <NavLink to="/" className="navbar-link">About</NavLink>
             </li>
+            <li className='navbar-item'>
+                <NavLink to="/resume" className="navbar-link">Resume</NavLink>
+            </li>
+            <li className='navbar-item'>
+                <NavLink to="/portfolio" className="navbar-link">Portfolio</NavLink>
+            </li>
+            <li className='navbar-item'>
+                <NavLink to="/contact" className="navbar-link">Contact</NavLink>
+            </li>
       </ul>
     </nav>
   )
--- src/page/About/About.jsx
+++ src/page/About/About.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
-
+import Service from '../Service'
+const servicesData = [
+  {
+    "title": "Web development",
+    "icon": "/image/icon-dev.svg",
+    "description": "High-quality development of sites at the professional level."
+  },
+  {
+    "title": "Mobile Development",
+    "icon": "/image/icon-app.svg",
+    "description": "Professional development of applications for iOS and Android."
+  },
+  {
+    "title": "Mentorship",
+    "icon": "/image/mentorship.png",
+    "description": "I love helping developers and learners improve their craft."
+  },
+  {
+      "title": "Web Design",
+      "icon": "/image/icon-design.svg",
+      "description": "The most modern and high-quality design made at a professional level."
+  }
+]
 const About = () => {
   return (
-    <div>
-      about
+    <div className='about active'>
+      <header>
+        <h2 className='h2 article-title'>About me</h2>
+      </header>
+      <section className='about-text'>
+        <p> I'm a Computer Science enthusiast from Morocco, working in Software and web development.<br></br>
+        I enjoy problem solving
+        I enjoy working with people, and I'm always looking for new challenges and opportunities.<br></br>
+        Feel free to explore my portfolio to discover my latest projects and learn more about my skills and experience in the field.</p>
+      </section>
+
+
+      <section className='service'>
+        <h2 className='h3 service-title'>What i'm doing</h2>
+        <ul className='service-list'>
+          {
+            servicesData.map((service, index)=>(
+              <Service key={index} title={service.title} icon={service.icon} description={service.description}/>
+            ))
+          }
+        </ul>
+      </section>
     </div>
   )
 }
