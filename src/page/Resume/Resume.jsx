5fc3f6c410a5a437f3bbd10fd8b2e5f8d1678498
othmane tozy <tozyothmane123@gmail.com>
2024-09-13 19:08:37 +0100

    new section experience

--- src/page/Resume/Resume.jsx
+++ src/page/Resume/Resume.jsx
@@ -27,6 +27,32 @@ const Resume = () => {
           description="science physique."
           />
         </ol>
+        <br />
+        <div className='title-wrapper'>
+          <div className='icon-box'>
+          <img src="/image/briefcase.png" alt="" width="24" />
+          </div>
+          <h3 className='h3'>Experience</h3>
+        </div>
+        <ol className='timeline-list'>
+          <Timeline 
+          title="R&D Engineer intern"
+          date="2019 - 2024"
+          description="Systems and Computers Engineering Department."
+          />
+          <Timeline 
+          title=" baccalauréat, spécialité science physique"
+          date="2018 - 2019"
+          description="science physique."
+          />
+        </ol>
+      </div>
+
+
+      <div className='skill'>
+          <h3 className='h3 skills-title'>My Skills</h3>
+          <ul className='skills-list content'>
+          </ul>
       </div>
     </section>
   )
