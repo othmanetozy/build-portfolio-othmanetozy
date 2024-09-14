ecdfd9bd61b98e97e0781c53da259d1cc778b569
othmane tozy <tozyothmane123@gmail.com>
2024-09-14 13:33:19 +0100

    projects

--- src/page/Portfolio/Portfolio.jsx
+++ src/page/Portfolio/Portfolio.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Portfolio = () => {
+    const [projects, setprojects] = useState([]);
+    const [filterProjects , setfilterProjects] = useState([])
+    const [selectCategory, setselectCategory] = useState('All') 
+
+    useEffect(()=>{
+        fetch('projects.json').then(res => res.json()).then(data=>{
+            setprojects(data);
+            setfilterProjects(data);
+        })
+    })
+
+
   return (
    <section>
-    
+    <header>
+        <h2 className='h2 article-title'>Portfolio</h2>
+    </header>
+    <ul>
+        <ul className='filter-list'>
+            {['All','Web Development','ReactJs','AngularJs','Loading...'].map(category =>(
+                <li key={category} className='filter-item'>
+                    <button className={category ===selectCategory ? 'active' : ''}>{category}</button>
+                </li>
+            ))}
+        </ul>
+    </ul>
+
+
+    <section className='projects'>
+            <ul className='project-list'>
+                {
+                    filterProjects.map(project => (
+                        <li key={project.id} className='project-item active'>
+                            <a href="#">
+                                <figure className='project-img'>
+                                    <img src={project.image} alt="" />
+                                </figure>
+                            </a>
+                        </li>
+                    ))
+                }
+            </ul>
+    </section>
+
    </section>
   )
 }
